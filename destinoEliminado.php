2cb221de16cdb51c90f86545fc3d22794cda62ab
Patoorlando2 <patoorlando2@gmail.com>
2023-07-04 20:27:54 -0300

    updating..

---
+++ destinoEliminado.php
@@ -0,0 +1,30 @@
+<?php
+    require 'config/config.php';
+    //inyección de dependencias
+    //require 'clases/Conexion.php';
+    //require 'clases/Destino.php';
+    $Destino = new Destino;
+    $check = $Destino->eliminarDestino();
+    
+    $css = 'danger';
+    $mensaje = 'No se pudo eliminar el destino.';
+    if( $check ){
+        $css = 'success';
+        $mensaje = 'Destino '.$Destino->getDestNombre().' eliminado correctamente.';
+    }
+    include 'includes/over-all-header.html';
+    include 'includes/nav.php';
+?>
+
+    <main class="container">
+
+        <h1>confirmación  de una baja de destino</h1>
+
+        <div class="alert alert-<?= $css ?> col-8 mx-auto">
+            <?= $mensaje ?> <a href="adminDestinos.php" class="btn btn-light">Volver a panel</a>
+        </div>
+    </main>
+
+<?php
+    include 'includes/footer.php';
+?>
